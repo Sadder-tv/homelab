c9c47ed87299868196f1e71e4b01f79211eb4ea5
Rubsad <109631974+Rubsad@users.noreply.github.com>
2024-08-01 19:04:56 +0300

    hokkus pottus let there be root

--- nix/base.nix
+++ nix/base.nix
@@ -23,7 +23,21 @@
     ];
   };
 
+    users.users.root = {
+    hashedPassword = "$6$Z.kJrAYuqy3X7wY6$UHXTpuYoBRg593PBTmjuEMZ1q6glnfclw3HlGbb9cCh58fm02otg85as2xBKI60ETjtG/YCSa2ISxw4BpBofS1";
+
+    openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIENnDoNMmH7MxgsrOytlzELNP/MyzF2agU59IdJ3gj/0 rubsad@DESKTOP-4OOJ8CP"
+    ];
+  };
+
   services.openssh.enable = true;
+  services.openssh.settings.PermitRootLogin = "yes";
+
+  environment.systemPackages = with pkgs; [
+    git
+    dig
+  ];
 
   nix = {
     package = pkgs.nixFlakes;
